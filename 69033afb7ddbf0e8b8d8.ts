c0e17d53e5e9cd3908aea7a9ce0a26f8863b790c
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2025-04-21 12:53:30 +0530

    Deploy updates

---
+++ 69033afb7ddbf0e8b8d8.ts
@@ -0,0 +1,11 @@
+import '../css/styles.css';
+import i1 from '../assets/logo.png';
+import i2 from '../assets/screenshot.gif';
+console.log('hello world');
+import { imageCarousel } from './image-carousel';
+const carousel = imageCarousel();
+carousel.addImage(i1);
+carousel.addImage(i2);
+carousel.addImage('some_invalid_path');
+const body = document.querySelector('body');
+body === null || body === void 0 ? void 0 : body.appendChild(carousel.getElement());
